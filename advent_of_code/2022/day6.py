6433ae384e32ffe91b1fed5660def69a3969e54c
Mike Szczys <szczys@hotmail.com>
2022-12-06 21:05:36 -0600

    Update to solve both day 6 puzzles

--- advent_of_code/2022/day6.py
+++ advent_of_code/2022/day6.py
@@ -1,22 +1,28 @@
-def get_input():
-    with open("day6_input.txt", "r") as f:
-        return f.read().splitlines()
+class elf_stream:
+    def __init__(self):
+        self.receive_buffer = ""
+        self.elfstream = self.get_input()[0]
 
-def got_preamble(new_char, buffer):
-    buffer += new_char
-    if len(buffer) > 3:
-        if len(set(buffer[-4:])) == 4:
-            return True, buffer
-    return False, buffer
+    def get_input(self):
+        with open("day6_input.txt", "r") as f:
+            return f.read().splitlines()
 
-def stream_test(source):
-    receive_buffer = ""
-    for c in source:
-        success, receive_buffer = got_preamble(c, receive_buffer)
-        if success:
-            print("Found preamble at:", len(receive_buffer))
-            return
-    print("No preamble found")
+    def got_preamble(self, new_char, unique):
+        self.receive_buffer += new_char
+        if len(self.receive_buffer) >= unique:
+            if len(set(self.receive_buffer[-unique:])) == unique:
+                return True
+        return False
 
-puzzle_stream = get_input()
-stream_test(puzzle_stream[0])
+    def stream_find_preamble(self, source, unique_count):
+        self.receive_buffer = ""
+        for c in source:
+            success = self.got_preamble(c, unique_count)
+            if success:
+                print("Found preamble at:", len(self.receive_buffer))
+                return
+        print("No preamble found")
+
+transmission = elf_stream()
+transmission.stream_find_preamble(transmission.elfstream, 4)
+transmission.stream_find_preamble(transmission.elfstream, 14)
