566d21d3f848cd5ba667f53c1b88bba355bd3547
Mike Szczys <szczys@hotmail.com>
2022-12-12 00:18:51 -0600

    Added solution to Day9, puzzle 2

--- advent_of_code/2022/day9.py
+++ advent_of_code/2022/day9.py
@@ -1,49 +1,58 @@
 from itertools import product
 
+class Pt:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
 class Snake:
-    def __init__(self, f_name):
+    def __init__(self, f_name, nodes=2):
         with open(f_name, "r") as f:
             self.moves = [[i.split(" ")[0], int(i.split(" ")[1])] for i in f.read().splitlines()]
-        self.hx = 0
-        self.hy = 0
-        self.tx = 0
-        self.ty = 0
-
+        self.knots = list()
+        for k in range(nodes):
+            self.knots.append(Pt(0,0))
         self.tail_locs = [[0,0]]
 
     lut = { 'R' : [1, 0], 'D' : [0, -1], 'L' : [-1, 0], 'U' : [0, 1] }
 
-    def move_head(self, dir):
-        self.hx += self.lut[dir][0]
-        self.hy += self.lut[dir][1]
+    def move_head(self, dir, head):
+        head.x += self.lut[dir][0]
+        head.y += self.lut[dir][1]
 
-    def must_move(self):
-        x = list(range(self.tx-1, self.tx+2))
-        y = list(range(self.ty-1, self.ty+2))
-        if (self.hx, self.hy) in product(x,y):
+    def must_move(self, head, tail):
+        x = list(range(tail.x-1, tail.x+2))
+        y = list(range(tail.y-1, tail.y+2))
+        if (head.x, head.y) in product(x,y):
             return False
         return True
 
-    def move_tail(self):
-        if self.must_move():
-            if self.hx > self.tx:
-                self.tx += 1
-            if self.hx < self.tx:
-                self.tx -= 1
-            if self.hy > self.ty:
-                self.ty += 1
-            if self.hy < self.ty:
-                self.ty -= 1
-            if [self.tx, self.ty] not in self.tail_locs:
-                self.tail_locs.append([self.tx, self.ty])
+    def move_tail(self, head, tail):
+        if self.must_move(head, tail):
+            if head.x > tail.x:
+                tail.x += 1
+            if head.x < tail.x:
+                tail.x -= 1
+            if head.y > tail.y:
+                tail.y += 1
+            if head.y < tail.y:
+                tail.y -= 1
 
     def process_moves(self):
         for move in self.moves:
             for i in range(move[1]):
-                self.move_head(move[0])
-                self.move_tail()
+                for k in range(len(self.knots)):
+                    if k == 0:
+                        self.move_head(move[0], self.knots[0])
+                    else:
+                        self.move_tail(self.knots[k-1], self.knots[k])
+                if [self.knots[-1].x, self.knots[-1].y] not in self.tail_locs:
+                    self.tail_locs.append([self.knots[-1].x, self.knots[-1].y])
         return len(self.tail_locs)
 
 s = Snake("day9_input.txt")
 # s = Snake("day9_test.txt")
 print("Answer 1:", s.process_moves())
+t = Snake("day9_input.txt", 10)
+# t = snake("day9_test2.txt", 10)
+print("Answer 2:", t.process_moves())
