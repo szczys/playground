9ca9ef7732e548383c6735437fadf2fd71a01202
Mike Szczys <szczys@hotmail.com>
2022-12-26 22:59:42 -0600

    add solution for Day 13, Puzzle 2
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- advent_of_code/2022/day13.py
+++ advent_of_code/2022/day13.py
@@ -8,6 +8,16 @@ class Packets:
         self.packets = [(json.loads(rows[i*3]), json.loads(rows[(i*3)+1])) for i in range(int(math.ceil(len(rows)/3)))]
         self.packets.insert(0, None) #Indexing is supposed to begin at 1
 
+        self.unsorted = list()
+        for i in range(1, len(self.packets)):
+            self.unsorted.append(self.packets[i][0])
+            self.unsorted.append(self.packets[i][1])
+        self.unsorted.append([[2]])
+        self.unsorted.append([[6]])
+
+        for i in self.unsorted:
+            print(i)
+
     def is_correct_order(self, t, indent=""):
         left = t[0]
         right = t[1]
@@ -60,6 +70,30 @@ class Packets:
                 return True
         return None # Returning True here will allow inner recursion to continue
 
+    def merge_sort(self, a):
+        if len(a) == 1:
+            return a
+        split = int(math.ceil(len(a)/2))
+        merged0 = self.merge_sort(a[:split])
+        merged1 = self.merge_sort(a[split:])
+
+        return self.merge(merged0, merged1)
+
+    def merge(self, a, b):
+        c = list()
+
+        while (a and b):
+            if self.is_correct_order((a[0],b[0])):
+                c.append(a.pop(0))
+            else:
+                c.append(b.pop(0))
+        while (a):
+            c.append(a.pop(0))
+        while (b):
+            c.append(b.pop(0))
+
+        return c
+
     def test_packets(self):
         sorted_correctly = list()
         for i in range(1,len(self.packets)):
@@ -68,11 +102,25 @@ class Packets:
             print()
             if x:
                 sorted_correctly.append(i)
-        print("Totals:", sum(sorted_correctly))
+        total = sum(sorted_correctly)
+        print("Totals:", total)
+        return total
+
+    def sort_all(self):
+        all_sorted = self.merge_sort(self.unsorted)
+        all_sorted.insert(0,None) # Adjust to index at 1 (not 0)
+        x = all_sorted.index([[2]])
+        y = all_sorted.index([[6]])
+        return x*y
 
 
 
 
 # p = Packets("day13_test.txt")
 p = Packets("day13_input.txt")
-p.test_packets()
+question_1 = p.test_packets()
+question_2 = p.sort_all()
+
+print()
+print ("Question #1 solution:", question_1)
+print ("Question #2 solution:", question_2)
