da7b0a650cf0c8f5f844b7a3e66d8f086c65fc16
Mike Szczys <szczys@hotmail.com>
2022-12-27 11:58:21 -0600

    add solution for Day 14, Puzzle 2
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- advent_of_code/2022/day14.py
+++ advent_of_code/2022/day14.py
@@ -15,11 +15,38 @@ class Rocks:
         self.max_x = max(allx)
         self.min_y = min(ally)
         self.max_y = max(ally)
-        self.grid_zero = self.min_x-1
+        self.grid = list()
+        self.grid_zero = 0
+        self.make_grid()
 
+    def is_overflowing(self):
+        return 'o' in self.grid[-1]
+
+    def print_grid(self):
+        for g in self.grid:
+            print(g)
+
+    def make_grid(self, infinite=None):
+        self.grid_zero = self.min_x-1
         width = (self.max_x-self.min_x)+3
         height = self.max_y+2
+
+        if infinite:
+            x = infinite
+            y = height
+            infinite_left = x-y
+            infinite_right = x+y
+            if infinite_left > self.min_x:
+                infinite_left = self.min_x
+            if infinite_right < self.max_x:
+                infinite_righ = self.max_x
+            infinite_width = infinite_right-infinite_left+3
+            width = infinite_width
+            self.grid_zero = infinite_left-1
         self.grid = ['.'*width for i in range(height)]
+        if infinite:
+            self.grid.append('#'*width)
+
         for v in self.vectors:
             for i in range(len(v)-1):
                 a,b = v[i:i+2]
@@ -30,12 +57,6 @@ class Rocks:
                         grid_col = col-self.grid_zero
                         self.grid[row] = self.grid[row][:grid_col] + '#' + self.grid[row][grid_col+1:]
 
-    def is_overflowing(self):
-        return 'o' in self.grid[-1]
-
-    def print_grid(self):
-        for g in self.grid:
-            print(g)
 
     def drop_sand(self, x, y):
         if y != self.max_y+1:
@@ -52,6 +73,7 @@ class Rocks:
         self.grid[y] = self.grid[y][:grid_col] + 'o' + self.grid[y][grid_col+1:]
 
     def hourglass(self, x, y):
+        self.make_grid()
         while not self.is_overflowing():
             self.drop_sand(x, y)
 
@@ -60,11 +82,26 @@ class Rocks:
             total += Counter(g)['o']
         return total-1
 
+    def infinite_hourglass(self, x, y):
+        self.make_grid(infinite=x)
+        while 'o' not in self.grid[0]:
+            self.drop_sand(x, y)
+
+        total = 0
+        for g in self.grid:
+            total += Counter(g)['o']
+        return total
+
 # r = Rocks("day14_test.txt")
 r = Rocks("day14_input.txt")
 
 print()
 answer_1 = r.hourglass(500,0)
 r.print_grid()
+
+answer_2 = r.infinite_hourglass(500,0)
+# r.print_grid()
+
 print()
 print("Answer #1:", answer_1)
+print("Answer #2:", answer_2)
