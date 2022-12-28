fbdfcf136bcc07fd075eb8be4baf7fb2f8d917c7
Mike Szczys <szczys@hotmail.com>
2022-12-28 00:23:26 -0500

    add solution for Day 15, Puzzle 2
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- advent_of_code/2022/day15.py
+++ advent_of_code/2022/day15.py
@@ -7,8 +7,20 @@ class Sensor:
         self.beacon_x = int(beacon_x)
         self.beacon_y = int(beacon_y)
         self.distance = abs(self.x - self.beacon_x) + abs(self.y - self.beacon_y)
+        self.y_start = self.y-self.distance
+        self.y_end = self.y+self.distance
+
     def __repr__(self):
-        return "Sensor: {},{} Beacon: {},{} Distance: {}".format(self.x, self.y, self.beacon_x, self.beacon_y, self.distance)
+        return "Sensor: {},{} Beacon: {},{} Distance: {} Y_Start: {} Y_End: {}".format(
+            self.x, self.y, self.beacon_x, self.beacon_y, self.distance,
+            self.y_start, self.y_end)
+
+    def get_spread(self, y):
+        if self.y_start <= y <= self.y_end:
+            offset = self.distance-abs(self.y - y)
+            return (self.x-offset, self.x+offset)
+        else:
+            return None
 
 class Survey:
     def __init__(self, f_name):
@@ -32,30 +44,78 @@ class Survey:
         offset = abs(target_y - sensor.y)
 
         if offset > sensor.distance:
-            return []
+            return set()
         elif offset == sensor.distance:
-            return [sensor.x]
+            return {sensor.x}
         else:
             spread = sensor.distance - offset
-            return range(sensor.x-spread, sensor.x+spread+1)
+            return set(range(sensor.x-spread, sensor.x+spread+1))
 
-    def calc_coverage(self, y):
+    def calc_coverage(self, y, return_sets=False):
         y_range = range(y-self.max, y+self.max+1)
-        covered = list()
-        beacons = list()
+        covered = set()
+        beacons = set()
         for s in self.sensors:
             if s.beacon_y == y:
-                beacons.append(s.beacon_x)
+                beacons.add(s.beacon_x)
             if s.y in y_range:
-                covered.extend(self.get_spread(y, s))
-        covered = set(covered)
+                covered = covered | self.get_spread(y, s)
+        if return_sets:
+            return covered,beacons
         for b in beacons:
             covered.discard(b)
         return len(covered)
 
+    def graph_coverage(self):
+        for j in range(0,23):
+            covered, beacons = self.calc_coverage(j, True)
+            out = ""
+            for i in range(-2,36):
+                if i in beacons:
+                    out += 'B'
+                elif i in covered:
+                    out += '#'
+                else:
+                    out += '.'
+            print(out)
+            valid_slice = out[2:23]
+            if '.' in valid_slice:
+                print(j, valid_slice.index('.'))
+
+    def find_unoccupied(self, limit_x, limit_y):
+        '''
+        1. Get y begin and end numbers for all sensors
+        2. use algorithm to get x begin and end numbers for any sensor in a given y
+        3. check to see if there are any gaps in the overlap
+        '''
+        for row in range(limit_y[0], limit_y[1]+1):
+            span_list = list()
+            for s in self.sensors:
+                span = s.get_spread(row)
+                if span:
+                    span_list.append(span)
+            span_list.sort()
+            gap_list = list()
+            parsed_list = [span_list[0]]
+            for sp in span_list[1:]:
+                left = parsed_list[-1][1]
+                right = sp[0]
+                if left < right-1:
+                    gap_list.append((left+1, right-1))
+                if sp[1] > left:
+                    parsed_list.append(sp)
+            if len(gap_list) > 0:
+                print("Found gap at row: {} x-value: {}".format(row, gap_list))
+                return ((gap_list[0][0]*4000000)+row)
+
 # s = Survey("day15_test.txt")
+# s.graph_coverage()
 # solution_1 = s.calc_coverage(10)
+# solution_2 = s.find_unoccupied((0,20), (0,20))
+
 s = Survey("day15_input.txt")
 solution_1 = s.calc_coverage(2000000)
+solution_2 = s.find_unoccupied((0,4000001), (0,4000001))
 
 print("Solution #1:", solution_1)
+print("Solution #2:", solution_2)
