a9741a3de07bf47a80ad72fdaf80959ac217bcbf
Mike Szczys <szczys@hotmail.com>
2024-12-26 13:42:35 -0600

    day2 puzzle1
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

---
+++ advent_of_code/2024/day2_puzzle1.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Report:
+    def __init__(self, f_name):
+        with open(f_name, "r") as f:
+            self.rows = f.read().splitlines()
+
+    def is_safe(self, values: List[int]) -> bool:
+        prev = values[0]
+        is_increasing = (values[1] > values [0])
+        for next in values[1:]:
+            if (next > prev) != is_increasing:
+                return False
+            if not (0 < abs(next - prev) < 4 ):
+                return False
+            prev = next
+
+        return True
+
+    def calculate(self):
+        count = 0
+        for r in self.rows:
+            report = [int(i) for i in r.split(' ')]
+            if self.is_safe(report):
+                count += 1
+
+        return count
+
+r = Report("day2_input1.txt")
+print(r.calculate())
