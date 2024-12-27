06dd00b6ac1f5661df001c8f3d76566bc3efbc61
Mike Szczys <szczys@hotmail.com>
2024-12-27 08:36:21 -0600

    day3 puzzle2
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

---
+++ advent_of_code/2024/day3_puzzle2.py
@@ -0,0 +1,31 @@
+import re
+
+from typing import List
+
+class Multiply:
+    def __init__(self, f_name):
+        with open(f_name, "r") as f:
+            self.data = f.read()
+
+        p = re.compile("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
+        self.commands = p.findall(self.data)
+
+    def process(self):
+        p = re.compile("\d{1,3}")
+        total = 0
+        skip = False
+        for c in self.commands:
+            if c == "do()":
+                skip = False
+            elif c == "don't()":
+                skip = True
+            elif skip:
+                continue
+            else:
+                nums = p.findall(c)
+                total += (int(nums[0]) * int(nums[1]))
+        return total
+
+
+m = Multiply("day3_input1.txt")
+print(m.process())
