f24c949ea1d3a76fd14db0e765e621b186b7e48b
Mike Szczys <szczys@hotmail.com>
2024-12-27 08:20:36 -0600

    day3 puzzle1
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

---
+++ advent_of_code/2024/day3_puzzle1.py
@@ -0,0 +1,23 @@
+import re
+
+from typing import List
+
+class Multiply:
+    def __init__(self, f_name):
+        with open(f_name, "r") as f:
+            self.data = f.read()
+
+        p = re.compile("mul\(\d{1,3},\d{1,3}\)")
+        self.commands = p.findall(self.data)
+
+    def process(self):
+        p = re.compile("\d{1,3}")
+        total = 0
+        for c in self.commands:
+            nums = p.findall(c)
+            total += (int(nums[0]) * int(nums[1]))
+        return total
+
+
+m = Multiply("day3_input1.txt")
+print(m.process())
