e4e4ef6ad1217a87d56f479a90e45afa372dd4c8
Mike Szczys <szczys@hotmail.com>
2024-12-29 16:31:35 -0600

    day11 puzzle2
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- advent_of_code/2024/day11_puzzle1.py
+++ advent_of_code/2024/day11_puzzle1.py
@@ -1,3 +1,7 @@
+from typing import List
+import time
+import threading
+
 class Stones:
     def __init__(self, f_name):
         self.filename = f_name
@@ -6,29 +10,55 @@ class Stones:
 
         self.line = [int(s) for s in self.rows[0].split(' ')]
 
-    def blink(self, line):
-        for i in range(len(line) - 1, -1, -1):
-            if line[i] == 0:
-                line[i] += 1
-            elif len(str(line[i])) % 2 == 0:
-                str_stone = str(line[i])
-                half_width = int(len(str_stone) / 2 )
-                left_stone = int(str_stone[:half_width])
-                right_stone = int(str_stone[half_width:])
+        self.max = 0
+        self.start_time = time.time()
+        self.solutions = list()
 
-                if i == len(line) - 1:
-                    line.append(right_stone)
-                else:
-                    line.insert(i + 1, right_stone)
+    def blink(self, num: int) -> List:
+        if num == 0:
+            return [1]
+        elif len(str(num)) % 2 == 0:
+            str_stone = str(num)
+            half_width = int(len(str_stone) / 2 )
+            left_stone = int(str_stone[:half_width])
+            right_stone = int(str_stone[half_width:])
 
-                line[i] = left_stone
-            else:
-                line[i] *= 2024
+            return [left_stone, right_stone]
+        else:
+            return [num * 2024]
+
+    def recurse(self, num: int, depth: int) -> int:
+        if depth == 0:
+            return 1
+
+        count = 0
+        line = self.blink(num)
+
+        depth -= 1
+        for n in line:
+            if n in self.solutions[depth]:
+                count += self.solutions[depth][n]
+                continue
+
+            solution = self.recurse(n, depth)
+            count += solution
+        self.solutions[depth + 1][num] = count
+        if count > self.max:
+            self.max = count
+        return count
+
+    def solve(self, num_set: List, blinks: int) -> int:
+        self.solutions = list()
+        for _ in range(blinks + 1):
+            self.solutions.append(dict())
+
+        count = 0
+        for n in num_set:
+            count += s.recurse(n, blinks)
+        return count
  
 s = Stones("day11_input1.txt")
-# print(' '.join([str(n) for n in s.line]))
 
-for _ in range(25):
-    s.blink(s.line)
-
-print("Total stones:", len(s.line))
+s.start_time = time.time()
+for i in [25, 75]:
+    print(f"Total stones after {i} blinks:", s.solve(s.line, i))
